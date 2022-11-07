f6786706db9ab1e43fbc2e813459e679e6df8638
wangjuanjuan20 <wangjuanjuan20@jd.com>
2022-11-07 17:57:07 +0800

    两种类型组件

---
+++ src/pages/classCom/classCom.jsx
@@ -0,0 +1,12 @@
+import './classCom.scss'
+import { Component } from 'react';
+
+class ClassCom extends Component(){
+   render(){
+       return <div className='p-class'>
+           我是一个类组件
+       </div>
+   }
+}
+
+export default ClassCom ;
---
+++ src/pages/funcom/funCom.jsx
@@ -0,0 +1,12 @@
+import './funCom.scss'
+
+function FunCom (){
+  return <div className = 'p-fun'>
+      我是一个函数组件；
+  </div>
+
+
+
+}
+
+export default FunCom ;
