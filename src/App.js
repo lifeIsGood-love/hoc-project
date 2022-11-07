ba328c466193e8a602e87c3a074ddcaba732ea23
wangjuanjuan20 <wangjuanjuan20@jd.com>
2022-11-07 17:06:29 +0800

    添加路由

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import React from 'react' ;
+import ClassCom from './pages/classCom'
+import FunCom from './pages/funCom'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom' ;
 
 function App() {
   return (
-    <div className="App">
-      <h1>测试高阶项目1</h1>
-    </div>
+    // <div className="App">
+      <HashRouter>
+        <Switch>
+          <Route path="/classCom" component={ClassCom} />
+          <Route path="/funCom" component={FunCom} />
+          <Redirect to={"/funCom"} />
+        </Switch>
+      </HashRouter>
+    // </div>
   );
 }
 
